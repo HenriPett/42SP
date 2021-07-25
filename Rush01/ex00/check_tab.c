ca3c07d9001d8d77cb34d904aed2312bbffef669
hnass-pe <hnass-pe@student.42sp.org.br>
2021-07-25 16:01:07 +0000

    rush01 att

--- Rush01/ex00/check_tab.c
+++ Rush01/ex00/check_tab.c
@@ -1,94 +1,99 @@
 #include <unistd.h>
-#include "empilha.h"
+#include "empilha_caixa.h"
 
-int coluna_eh_valida(char tabela[TAMANHO][TAMANHO], t_parametros *parametros, int x)
+int	coluna_eh_valida(char tabela[TAM][TAM],
+	t_parametros *parametros, int x)
 {
-    int i;
-    int maior_tamanho;
-    int visualizacoes;
+	int	i;
+	int	maior_tamanho;
+	int	visualizacoes;
 
-    maior_tamanho = 0;
-    visualizacoes = 0;
-    i = -1;
-    
-    while (++i < TAMANHO)
-    {
-        visualizacoes += tabela[i][x] > maior_tamanho;
-        maior_tamanho = ((tabela[i][x] > maior_tamanho) ? tabela[i][x] : maior_tamanho);
-    }
-    if (visualizacoes != parametros->acima[x])
-        return (0);
-    maior_tamanho = 0;
-    visualizacoes = 0;
-    while (--i >= 0)
-    {
-        visualizacoes += tabela[i][x] > maior_tamanho;
-        maior_tamanho = ((tabela[i][x] > maior_tamanho) ? tabela[i][x] : maior_tamanho);
-    }
-    if(visualizacoes != parametros->abaixo[x])
-        return (0);
-    return (1);
+	maior_tamanho = 0;
+	visualizacoes = 0;
+	i = -1;
+	while (++i < TAM)
+	{
+		visualizacoes += tabela[i][x] > maior_tamanho;
+		if (tabela[i][x] > maior_tamanho)
+			maior_tamanho = tabela[i][x];
+	}
+	if (visualizacoes != parametros->acima[x])
+		return (0);
+	maior_tamanho = 0;
+	visualizacoes = 0;
+	while (--i >= 0)
+	{
+		visualizacoes += tabela[i][x] > maior_tamanho;
+		if (tabela[i][x] > maior_tamanho)
+			maior_tamanho = tabela[i][x];
+	}
+	if (visualizacoes != parametros->abaixo[x])
+		return (0);
+	return (1);
 }
 
-int linha_eh_valida(char tabela[TAMANHO][TAMANHO], t_parametros *parametros, int y)
+int	linha_eh_valida(char tabela[TAM][TAM],
+	t_parametros *parametros, int y)
 {
-    int i;
-    int maior_tamanho;
-    int visualizacoes;
+	int	i;
+	int	maior_tamanho;
+	int	visualizacoes;
 
-    maior_tamanho = 0;
-    visualizacoes = 0;
-    i = -1;
-    while (++i < TAMANHO)
-    {
-        visualizacoes += tabela[y][i] > maior_tamanho;
-        maior_tamanho = ((tabela[y][i] > maior_tamanho) ? tabela[y][i] : maior_tamanho);
-    }
-    if(visualizacoes != parametros->esquerda[y])
-        return 0;
-    maior_tamanho = 0;
-    visualizacoes = 0;
-    while (--i >= 0){
-        visualizacoes += tabela[y][i] > maior_tamanho;
-        maior_tamanho = ((tabela[y][i] > maior_tamanho) ? tabela[y][i] : maior_tamanho);
-    }
-    if(visualizacoes != parametros->direita[y])
-        return 0;
-    return 1;
+	maior_tamanho = 0;
+	visualizacoes = 0;
+	i = -1;
+	while (++i < TAM)
+	{
+		visualizacoes += tabela[y][i] > maior_tamanho;
+		if (tabela[y][i] > maior_tamanho)
+			maior_tamanho = tabela[y][i];
+	}
+	if (visualizacoes != parametros->esquerda[y])
+		return (0);
+	maior_tamanho = 0;
+	visualizacoes = 0;
+	while (--i >= 0)
+	{
+		visualizacoes += tabela[y][i] > maior_tamanho;
+		if (tabela[y][i] > maior_tamanho)
+			maior_tamanho = tabela[y][i];
+	}
+	if (visualizacoes != parametros -> direita[y])
+		return (0);
+	return (1);
 }
 
-int pode_colocar_valor (char tabela[TAMANHO][TAMANHO], int x, int y, int valor)
+int	pode_colocar_valor(char tabela[TAM][TAM], int x, int y, int valor)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    while (++i < TAMANHO)
-    {
-        if(tabela[y][i] == valor || tabela[i][x] == valor)
-        return 0;
-    }
-    return 1;
+	i = -1;
+	while (++i < TAM)
+	{
+		if (tabela[y][i] == valor || tabela[i][x] == valor)
+			return (0);
+	}
+	return (1);
 }
 
-int procura_caso_vazio (char tabela[TAMANHO][TAMANHO], int *x, int *y)
+int	procura_caso_vazio(char tabela[TAM][TAM], int	*x, int	*y)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = -1;
-    while (++i < TAMANHO)
-    {
-        j = -1;
-        while (++j < TAMANHO)
-        {
-            if(tabela[i][j] == 0)
-            {
-                *x = j;
-                *y = i;
-                return 1;
-            }
-        }
-        
-    }
-    return 0;
+	i = -1;
+	while (++i < TAM)
+	{
+		j = -1;
+		while (++j < TAM)
+		{
+			if (tabela[i][j] == 0)
+			{
+				*x = j;
+				*y = i;
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
--- Rush01/ex00/main.c
+++ Rush01/ex00/main.c
@@ -1,103 +1,101 @@
 #include <unistd.h>
-#include "empilha.h"
+#include "empilha_caixa.h"
 
-int imprime_tabela(char tabela[TAMANHO][TAMANHO])
+int	imprime_tabela(char tabela[TAM][TAM])
 {
-    int y;
-    int x;
+	int	y;
+	int	x;
 
-    y = -1;
-    while (++y < TAMANHO)
-    {
-        x = -1;
-        while (++x < TAMANHO)
-        {
-            write(1, &tabela[y][x], 1);
-            if (x != (TAMANHO - 1))
-                write(1, " ", 1);
-        }
-        write(1, "\n", 1);
-    }
-    return 1;
+	y = -1;
+	while (++y < TAM)
+	{
+		x = -1;
+		while (++x < TAM)
+		{
+			write(1, &tabela[y][x], 1);
+			if (x != (TAM - 1))
+				write(1, " ", 1);
+		}
+		write(1, "\n", 1);
+	}
+	return (1);
 }
 
-int resolucao(char tabela[TAMANHO][TAMANHO], t_parametros *parametros)
+int	resolucao(char tabela[TAM][TAM], t_parametros *parametros)
 {
-    int x;
-    int y;
-    int valor;
+	int	x;
+	int	y;
+	int	valor;
 
-    if(procura_caso_vazio(tabela, &x, &y))
-    {
-        valor = '0';
-        while (++valor <= (TAMANHO + '0'))
-        {
-            if(pode_colocar_valor(tabela, x, y, valor))
-            {
-                tabela[y][x] = valor;
-                if(x == (TAMANHO -1) && !linha_eh_valida(tabela, parametros, y))
-                    continue;
-                if(y == (TAMANHO - 1) && !coluna_eh_valida(tabela, parametros, x))
-                    continue;
-                if(resolucao(tabela, parametros) == 1)
-                    return 1;
-            }
-        }
-        tabela[y][x] = 0;
-        return 0;
-    }
-    return 1;
+	if (procura_caso_vazio(tabela, &x, &y))
+	{
+		valor = '0';
+		while (++valor <= (TAM + '0'))
+		{
+			if (pode_colocar_valor(tabela, x, y, valor))
+			{
+				tabela[y][x] = valor;
+				if (x == (TAM - 1) && !linha_eh_valida (tabela, parametros, y))
+					continue ;
+				if (y == (TAM - 1) && !coluna_eh_valida(tabela, parametros, x))
+					continue ;
+				if (resolucao(tabela, parametros) == 1)
+					return (1);
+			}
+		}
+		tabela[y][x] = 0;
+		return (0);
+	}
+	return (1);
 }
 
-int argumento_eh_valido(char *av, t_parametros *parametros)
+int	argumento_eh_valido(char *av, t_parametros *parametros)
 {
-    int i;
-    char *ptr_parametros;
+	int		i;
+	char	*ptr_parametros;
 
-    i = -1;
-    ptr_parametros = &parametros->acima[0];
-    while (av[++i] != '\0')
-    {
-        if(i % 2 != 0 && av[i] == ' ')
-            continue;
-        if(i % 2 != 0 && av[i] != ' ')
-            return 0;
-        *ptr_parametros = av[i] - '0';
-        ptr_parametros++;
-    }
-    if(TAMANHO == 4 && i != 31)
-        return 0;
-    if(TAMANHO == 6 && i != 47)
-        return 0;
-    return 1;
+	i = -1;
+	ptr_parametros = &parametros->acima[0];
+	while (av[++i] != '\0')
+	{
+		if (i % 2 != 0 && av[i] == ' ')
+			continue ;
+		if (i % 2 != 0 && av[i] != ' ')
+			return (0);
+		*ptr_parametros = av[i] - '0';
+		ptr_parametros++;
+	}
+	if (TAM == 4 && i != 31)
+		return (0);
+	if (TAM == 6 && i != 47)
+		return (0);
+	return (1);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_parametros    parametros;
-    char tabela[TAMANHO][TAMANHO];
-    int i;
-    int j;
+	t_parametros	parametros;
+	char			tabela[TAM][TAM];
+	int				i;
+	int				j;
 
-    if (ac != 2 || !argumento_eh_valido(av[1], &parametros))
-    {
-        write(1, "Error\n", 6);
-        return 0;
-    }
-    i = -1;
-    while (++i < TAMANHO)
-    {
-        j = -1;
-        while (++j < TAMANHO)
-        {
-            tabela[i][j] = 0;
-        }
-    }
-    if (!resolucao(tabela, &parametros))
-    {
-        write(1, "Error\n", 6);
-        return 0;
-    }
-    imprime_tabela(tabela);
-    return 1;
+	if (ac != 2 || !argumento_eh_valido(av[1], &parametros))
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	i = -1;
+	while (++i < TAM)
+	{
+		j = -1;
+		while (++j < TAM)
+			tabela[i][j] = 0;
+	}
+	if (!resolucao(tabela, &parametros))
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	imprime_tabela(tabela);
+	return (1);
 }
